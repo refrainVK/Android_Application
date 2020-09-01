a7a9e9dbd3d7ca5794fd15d9e04924591350b752
Hongyu Su <hongyusu@tencent.com>
2020-09-01 17:18:09 -0400

    Create push util

--- app/src/main/java/com/example/myapplication/data/LoginDataSource.java
+++ app/src/main/java/com/example/myapplication/data/LoginDataSource.java
@@ -13,11 +13,9 @@ public class LoginDataSource {
 
         try {
             // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+            LoggedInUser user =
+                    new LoggedInUser(password.toString(),username);
+            return new Result.Success<>(user);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,1 @@
 This is my sample project for CS8803
-
-test commit
